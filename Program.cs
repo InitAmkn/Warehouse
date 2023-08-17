3efd3b09fa8a66542f235a795d167cad2cdc73e0
Alexander Mamykin <109105189+InitAmkn@users.noreply.github.com>
2023-08-17 22:25:26 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Warehouse
+{
+    internal class Program
+    {
+        private String connectionString = ConfigurationManager.ConnectionStrings["WarehouseDB.mdf"].ConnectionString;
+        private SqlConnection connection = null;
+        static void Main(string[] args)
+        {
+            
+
+        }
+    }
+}
---
+++ model/Place.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse.model
+{
+    internal abstract class Place
+    {
+        protected double length { set; get; }
+        protected double width { set; get; }
+        protected double height { set; get; }
+
+    }
+}
